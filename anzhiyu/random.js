cd1d360fb49341742892d051c068e74fa025d99b
lxz <2973024995@qq.com>
2025-04-05 19:44:46 +0800

    Site updated: 2025-04-05 19:44:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/04/hello-world/","2025/04/05/mysql索引分类/"];function toRandomPost(){
+var posts=["2025/04/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
